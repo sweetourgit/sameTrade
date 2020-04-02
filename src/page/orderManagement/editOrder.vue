b293f0141a8977322b30898f843b45176ff3b26b
331450663 <331450663@qq.com>
2020-04-02 15:20:32 +0800

    同业产品预订修改

--- src/page/orderManagement/editOrder.vue
+++ src/page/orderManagement/editOrder.vue
@@ -30,8 +30,8 @@
              </el-table-column>
              <el-table-column label="签署状态" min-width="120">
                 <template slot-scope="scope">
-                     <span v-if="scope.row.orderStatus===1||scope.row.orderStatus===7||scope.row.orderStatus===9||scope.row.orderStatus===1||scope.row.orderStatus===0">未签</span>
-                     <span v-if="scope.row.orderStatus===2||scope.row.orderStatus===3||scope.row.orderStatus===4||scope.row.orderStatus===5||scope.row.orderStatus===6||scope.row.orderStatus===8">已签</span>
+                     <span v-if="orderForm.orderStatus===1||orderForm.orderStatus===7||orderForm.orderStatus===9||orderForm.orderStatus===10">未签</span>
+                     <span v-if="orderForm.orderStatus===2||orderForm.orderStatus===3||orderForm.orderStatus===4||orderForm.orderStatus===5||orderForm.orderStatus===6||orderForm.orderStatus===8">已签</span>
                 </template>
              </el-table-column>
              <el-table-column prop="idCard" label="操作" min-width="120">
